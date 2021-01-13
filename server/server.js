4c5c0f1baa06996c485dd817581831778ad0371e
Georgi Tsanev <cologneto@gmail.com>
2021-01-13 14:39:09 +0200

    POST request endpoint end point added for searching.

--- server/server.js
+++ server/server.js
@@ -3,32 +3,49 @@ const bodyParser = require("body-parser");
 const sql = require("./models/db.js");
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3030;
+
+
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/search", (req, res) => {
+app.post("/search", (req, res) => {
+    const text = req.body.text;
+    const limit = req.body.limit;
+
     sql.query("SELECT CompanyName, " +
-                    "CompanyNumber, " +
-                    "RegAddress.AddressLine1, " +
-                    "RegAddress.AddressLine2" +
-                    "RegAddress.PostTown" +
-                    "RegAddress.PostCode" +
+                    "CompanyNumber," +
+                    "POBox," +
+                    "AddressLine1," +
+                    "AddressLine2," +
+                    "PostTown," +
+                    "County," +
+                    "Country" +
         " FROM companies WHERE MATCH (CompanyName)\n" +
-        "    AGAINST ('+receipt bank' IN BOOLEAN MODE)", (err, result) => {
+        " AGAINST ('+" + text + "' IN BOOLEAN MODE)" +
+        "LIMIT " + limit.start + ", " + limit.end , (err, result) => {
+        
+        result.map((company) => {
+            for (const property in company) {
+                company[property] = company[property].replace(/["]+/g, '');
+            }
+
+            return company;
+        });
+        
         if (err) {
             console.log("error: ", err);
         }
 
         console.log("pesho: ", result);
-        res.json({ message: result });
+        res.json({ data: result });
     });
 
 
 });
 
-app.listen(3030, () => {
+app.listen(port, () => {
     console.log("Server is running on port " + port + "." );
 });
